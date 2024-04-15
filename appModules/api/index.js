723f3342e2e80e8e937b262e4f0d8c9845d72f83
Yachilenok <gohasav@icloud.com>
2024-04-15 21:03:24 +0300

    commit

---
+++ appModules/api/index.js
@@ -0,0 +1,4 @@
+const { endpoints, WEIGHT } = require("./config");
+const { getData, getRandomGame } = require("./api-utils");
+
+module.exports = { getData, endpoints, WEIGHT, getRandomGame };
---
+++ controllers/index.js
@@ -0,0 +1,11 @@
+const defaultRouteController = require("./default");
+const mainRouteController = require("./main");
+const gameRouteController = require("./game");
+const voteRouteController = require("./vote");
+
+module.exports = {
+  defaultRouteController,
+  mainRouteController,
+  gameRouteController,
+  voteRouteController,
+};
